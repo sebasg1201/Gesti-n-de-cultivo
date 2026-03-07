b6984a9fc3cb6e53b88b3e32bc41f9503d62d3a7
Stevan <bastobrayan246@gmail.com>
2026-03-07 09:06:23 -0500

    solucion

--- resources/views/SuperAdmin/index.blade.php
+++ resources/views/SuperAdmin/index.blade.php
@@ -227,7 +227,7 @@
                                         <button type="button"
                                             data-empresa="{{ json_encode($empresa) }}"
                                             onclick="openModal(JSON.parse(this.getAttribute('data-empresa')))"
-                                            class="p-2 text-green-600 hover:bg-green-100 rounded-lg transition-colors cursor-pointer" title="Ver Detalles">
+                                            class="p-2 text-green-400 hover:bg-green-100 rounded-lg transition-colors cursor-pointer" title="Ver Detalles">
                                             <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
                                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z" />
