7d3205f9d0f986c43f770aa7b6d0c8cd4d2443b6
sebasg1201 <sebastiangarciaalvarez123@gmail.com>
2026-03-20 14:06:05 -0500

    Un

---
+++ database/migrations/2026_03_19_141013_add_timestamps_to_insumo_and_terreno.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('insumo', function (Blueprint $table) {
+            $table->timestamps();
+        });
+
+        Schema::table('terreno', function (Blueprint $table) {
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('insumo', function (Blueprint $table) {
+            $table->dropTimestamps();
+        });
+
+        Schema::table('terreno', function (Blueprint $table) {
+            $table->dropTimestamps();
+        });
+    }
+};
